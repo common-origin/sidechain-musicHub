814f778c30ba1096d2015a7816dd0dca546ad4da
sidechain-studio <studio@sidechain.studio>
2017-03-11 19:14:34 +1100

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
+const path = require('path');
+
 module.exports = {
   context: __dirname + "/app",
 
-  entry: "./js/app.js",
+  entry: {
+  	javascript: "./js/app.js",
+  	html: "./index.html",
+  },
 
   output: {
     filename: "app.js",
     path: __dirname + "/dist",
-  }
-};
+  },
+
+  resolve: {
+    extensions: ['[]', '.js', '.jsx', '.json'],
+    root: path.resolve(__dirname, './app/js'),
+  },
+  
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ["react-hot-loader", "babel-loader"],
+      },
+      {
+		    test: /\.html$/,
+		    loader: "file-loader?name=[name].[ext]",
+		  },
+    ],
+  },
+}
